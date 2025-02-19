611858ed3133ddeed285fc6d9176d961a8d2619b
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-02-19 12:19:28 +0300

    Remove preloader animation code from main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
 import './sass/style.scss'
 
-gsap.fromTo("path", 
-    { drawSVG: "0%" }, 
-    { drawSVG: "100%", duration: 2, ease: "power2.out", onComplete: hidePreloader }
-  );
 
-  function hidePreloader() {
-    gsap.to("#preloader", { opacity: 0, duration: 0.5, onComplete: () => document.getElementById("preloader").remove() });
-  }
 
 
 
